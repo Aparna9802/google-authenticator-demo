0d11ba7f46c736306cc11145da305174cba5324a
Vlad M <vlad@cryptanite.com>
2019-04-12 15:36:41 +0300

    init commit

---
+++ src/main/java/com/vlad9pa/googleauthdemo/GoogleAuthDemoApplication.java
@@ -0,0 +1,13 @@
+package com.vlad9pa.googleauthdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleAuthDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleAuthDemoApplication.class, args);
+    }
+
+}
